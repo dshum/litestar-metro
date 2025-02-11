a3f2e40447d8d9478c28831c988a20a6be9c2ea1
Denis Shumeev <denis-shumeev@yandex.ru>
2025-02-12 02:21:25 +0300

    Fix docker config.

--- app/settings.py
+++ app/settings.py
@@ -14,7 +14,7 @@ class AppSettings(BaseSettings):
 
     DEBUG: bool = False
     REQUEST_MAX_BODY_SIZE: int = 10_000_000
-    DEFAULT_LOCALE: str = "en_US"
+    DEFAULT_LOCALE: str = "ru_RU"
 
 
 class DatabaseSettings(BaseSettings):
